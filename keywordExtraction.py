cd450ef5ea32ae1d6a06998dc5fb1cdfabc37516
Prashant Pandey <prashantppmsd2405@gmail.com>
2020-06-22 18:57:32 +0530

    Update keywordExtraction.py

--- keywordExtraction.py
+++ keywordExtraction.py
@@ -41,39 +41,20 @@ def GetKeywords(input_list):
 # model --> The keyword extraction model to be used. Options are Rake or MonkeyLearn
 # API_token --> API token for MonkeyLearn. Default value is mine. Optional argument, needed only if model is MonkeyLearn Limited to 300 queries per month in the free account. 
 # Return value: consolidated--> A dictionary containing paragraph string (key--> paragraph), paragraph ID(key--> para_ID) and keywords (key--> keywords)
-def createMasterDict(input_list,model, API_token ='06f65ed21f102ecd78427152869791eb66fe5772' ):
-    if model in ["MonkeyLearn","ML","ml","monkeylearn"]:
-        ml = MonkeyLearn(API_token)    
-        model_id = 'ex_YCya9nrn'
-        consolidated=[]
-        j = 1
-        for data in input_list:
-            features = {}
-            features['paragraph']=data
-            features['para_ID']=j
-            j+=1
-            result = ml.extractors.extract(model_id, [data])
-            extrac=result.body[0]['extractions']
-            keywords=[]
-            for i in range(len(extrac)):
-                keywords.append(extrac[i]['parsed_value'])
-            features['keywords']=keywords
-            consolidated.append(features)
-        return consolidated
-    elif model in ["rake","Rake"]:
-        r = Rake() # Uses stopwords for english from NLTK, and all puntuation characters.
-        consolidated = []
-        k=1
-        for para in input_list:
-            features_={}
-            features_['para_ID']=k
-            features_['paragraph']=para
-            k+=1
-            r.extract_keywords_from_text(para)
-            #print(r.get_ranked_phrases()) # To get keyword phrases ranked highest to lowest.
-            features_['keywords']=r.get_ranked_phrases()
-            consolidated.append(features_)
-        return consolidated
+def createMasterDict(input_list):
+    r = Rake() # Uses stopwords for english from NLTK, and all puntuation characters.
+    consolidated = []
+    k=1
+    for para in input_list:
+        features_={}
+        features_['para_ID']=k
+        features_['paragraph']=para
+        k+=1
+        r.extract_keywords_from_text(para)
+        #print(r.get_ranked_phrases()) # To get keyword phrases ranked highest to lowest.
+        features_['keywords']=r.get_ranked_phrases()
+        consolidated.append(features_)
+    return consolidated
 
 # Running the search for a keyword
 # master_dict --> dictionary with keywords, paragraph_IDs and paragraphs; The create_master_dict() returns the dictionary to be used here
@@ -84,25 +65,6 @@ def keywordSearch(master_dict,keyword_list):
     for word in keyword_list:
         for item in master_dict:
             if word in item['keywords']:
-                text+=item['paragraph']
+                text+=item['paragraph']  #Generating the paragraphs that contains the keyword provided by user
     return text
 
-
-#help
-def helps():
-    print("Available functions:\n")
-    
-    print("\npreprocess_text(filepath): Opening the text file and splitting it into a list of strings\n")
-    print("filepath --> path to input text file (string)\nReturn value: data_split--> a list of strings, each string contains a paragraph\n")
-    
-    print("\ncreate_master_dict(input_list,model, API_token): Creating a dictionary containing paragraphs linked to the keywords")
-    print("input_list --> List of paragraph strings")
-    print("model --> The keyword extraction model to be used. Options are Rake or MonkeyLearn")
-    print("API_token --> API token for MonkeyLearn. Default value is mine. Optional argument, needed only if model is MonkeyLearn Limited to 300 queries per month in the free account.")
-    print("Return value: consolidated--> A dictionary containing paragraph string (key--> paragraph), paragraph ID(key--> para_ID) and keywords (key--> keywords)\n")
-
-    print("\nkeyword_search(master_dict,keyword_list): Running the search for a keyword")
-    print("master_dict --> dictionary with keywords, paragraph_IDs and paragraphs; The create_master_dict() returns the dictionary to be used here")
-    print("keyword_list --> A list of keywords to search for")
-    print("Return value: para_ID_list--> A list of pargraph IDs that contain the searched keywords. This can be used to access the required paragraphs from the input dictionary\n")
-    return 0
